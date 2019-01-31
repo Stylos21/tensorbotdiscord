3ee498387a771c0cea363c4ebb8a297062d64495
elementzprojects <33937944+elementzprojects@users.noreply.github.com>
2019-01-31 17:01:21 -0500

    Create hello.js

---
+++ commands/hello.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+   message.channel.send("wus poppin' fam?")
+};
+
+module.exports.help = {
+  name: "hello"
+}
