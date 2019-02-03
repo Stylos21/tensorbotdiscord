2244d93166490797982cb15ddd046a609701381e
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-02-03 11:36:58 -0500

    Add comments
    #4

--- functions/one_letter_username.js
+++ functions/one_letter_username.js
@@ -1,4 +1,6 @@
+// Give a special role to users with one-letter long nicknames
 function oneLetterUsername(oM, nM) {
+      // Create 'One-letter nickname club' role with a random color if it does not already exist
       var role = createNewRole(nM.guild, {
             name: 'One-letter nickname club',
             color: 'RANDOM',
@@ -6,17 +8,22 @@ function oneLetterUsername(oM, nM) {
       });
       Promise.resolve(role).then(
             (role) => {
+                  // If nickname is exactly one character long, add role to user
                   if (nM.displayName.length === 1) {
                         nM.addRole(role);
-                  } else {
+                  }
+                  // Otherwise, remove the role if they already have it
+                  else {
                         nM.removeRole(role);
                   }
             }
       );
 }
 
+// If a guild member's nickname changes, check if they qualify for the role
 bot.on('guildMemberUpdate', (oldMember, newMember) => oneLetterUsername(oldMember, newMember))
 
+// Export run function
 module.exports = {
       'run': oneLetterUsername
 };
