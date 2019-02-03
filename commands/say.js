efa5b658bb874271fa3dcd8344a9b7594a0ed70f
elementzprojects <33937944+elementzprojects@users.noreply.github.com>
2019-02-03 09:36:25 -0500

    Create say.js
    Made a command to make the bot talk.

---
+++ commands/say.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+module.exports.run = (bot, message, args) => {
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply('na bro');
+
+    message.delete();
+    message.channel.send(args.join(' ').replace("@everyone", "everyone").replace("@here", "here")).catch(console.error);
+}
+
+
+module.exports.help = {
+    name: "say"
+}
