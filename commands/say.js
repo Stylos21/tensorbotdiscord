ecd25d62ac5a0d24f94c273f493d55367c6417bb
elementzprojects <33937944+elementzprojects@users.noreply.github.com>
2019-02-03 09:37:47 -0500

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 module.exports.run = (bot, message, args) => {
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply('na bro');
-
+    console.log('hello') //test
     message.delete();
     message.channel.send(args.join(' ').replace("@everyone", "everyone").replace("@here", "here")).catch(console.error);
 }
