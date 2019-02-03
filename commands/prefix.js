ce398aacc65022afcf3826f8ea3c4d28e71b127f
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-02-03 15:51:35 -0500

    Add command functionality (store prefix using Firebase)

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,3 +1,10 @@
 const firebase = require('firebase-admin');
 const Discord = require('discord.js');
 var database = firebase.database();
+
+module.exports.run = async (bot, message, args) => {
+      database.ref('servers/' + message.guild.id + '/settings/prefix').set(args[0]).then(
+            () => message.channel.send('Server command prefix updated to `' + args[0] + '`')
+      );
+};
+
