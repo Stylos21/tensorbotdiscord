4fe6111284dc61644cc0357302a290bf21a56b1b
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-02-03 11:41:12 -0500

    Add comments
    #4

--- functions/new_member.js
+++ functions/new_member.js
@@ -1,6 +1,9 @@
+// Create a special role for every 100th member to join the server
 bot.on("guildMemberAdd", (member) => {
       var guild = member.guild;
+      // Check if new member is the 100th, 200th, 300th, etc. member to join the server
       if (guild.memberCount % 100 == 0) {
+            // Create a new role for the member (if nonexistent)
             var role = createNewRole(guild, {
                   name: guild.memberCount + 'th',
                   color: 'RANDOM',
@@ -10,6 +13,7 @@ bot.on("guildMemberAdd", (member) => {
 		// https://stackoverflow.com/a/27760489
 		Promise.resolve(role).then(
 			(role) => {
+                        // Add the role to the member then notify them via a public message
 				member.addRole(role).then(
 					() => guild.systemChannel.send('Congratulations ' + member + ', you are the ' + guild.memberCount + 'th person to join the `' + guild.name + '` server!')
 				);
