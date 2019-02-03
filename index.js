6a7915dbbdc278f814d9378487a26f8fec3f2fb6
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-02-02 22:13:59 -0500

    Post xkcd machine learning comic at random intervals (on average once every week)

--- index.js
+++ index.js
@@ -58,4 +58,16 @@ bot.on("guildMemberAdd", (member) => {
       }
 });
 
+function xkcd() {
+      bot.guilds.forEach(
+            (guild) => {
+                  if (Math.random() < 1 / 24 / 7) {
+                        guild.systemChannel.send('Obligatory xkcd: https://xkcd.com/1838/');
+                  }
+            }
+      );
+}
+
+var interval = setInterval(xkcd, 1000 * 60 * 60);
+
 bot.login(auth.token);
