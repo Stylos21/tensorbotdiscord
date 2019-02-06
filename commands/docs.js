51e4e4a1eca1b886e076b4f5d1c0b3c49983ca1e
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-02-05 19:01:47 -0500

    Add documentation reference command functionality and embed
    Documentation is looked up in JSON file, then a custom embed is created with information about that TensorFlow functionality and its parameters and usage.

--- commands/docs.js
+++ commands/docs.js
@@ -14,3 +14,42 @@ function findDoc(name){
       });
       return ref;
 }
+
+module.exports.run = async (bot, message, args) => {
+      // const section = docs.headings.find((x) => {
+      //       x.subheadings.includes((y) => {
+      //             y.symbols.find((z) => {
+      //                   z.symbolName == args[0]
+      //             })
+      //       })
+      // });
+      const doc = findDoc(args[0]);
+      if (doc) {
+            const fields = [];
+            doc.parameters.forEach((param) => {
+                  fields.push({
+                        name: param.name,
+                        value: param.documentation + "\nType: " + param.type + "\nOptional: " + param.optional
+                  });
+            });
+            fields.push({
+                  name: "Source",
+                  value: doc.githubUrl
+            });
+            message.channel.send({embed: {
+                  color: 3447003,
+                  author: {
+                        name: "TensorFlow Documentation"
+                  },
+                  title: doc.symbolName,
+                  url: "https://js.tensorflow.org/api/latest/index.html#" + doc.urlHash,
+                  description: doc.documentation,
+                  fields: fields,
+                  timestamp: new Date(),
+                  footer: {
+                        icon_url: bot.user.avatarURL,
+                        text: "TensorBot"
+                  }
+            }});
+      }
+};
