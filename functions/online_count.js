bca7ebad222de64312f137424fcc03a778fcbf6e
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-02-05 22:18:32 -0500

    Add tracking function

--- functions/online_count.js
+++ functions/online_count.js
@@ -1,3 +1,14 @@
 const firebase = require('firebase-admin');
 const Discord = require('discord.js');
 var database = firebase.database();
+
+function updateMembers(oldMember, newMember) {
+      var guild = newMember.guild;
+      var onlineCount = guild.members.filter(x => x.presence.status === 'online').size;
+      database.ref('servers/' + guild.id + '/online/states/' + Date.now()).set(onlineCount);
+      database.ref('servers/' + guild.id + '/online/record').once('value').then((record) => {
+            if (!record.val() || onlineCount > record.val()) {
+                  database.ref('servers/' + guild.id + '/online/record').set(onlineCount);
+            }
+      });
+}
