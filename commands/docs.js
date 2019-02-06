f09505fb739b227da4546aae53ae76f70ab5fe41
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-02-05 19:00:18 -0500

    Add findDoc function

--- commands/docs.js
+++ commands/docs.js
@@ -1,2 +1,16 @@
 const Discord = require('discord.js');
 const docs = require('../docs.json')
+
+function findDoc(name){
+      var ref = false;
+      docs.headings.forEach((x) => {
+            x.subheadings.forEach((y) => {
+                  y.symbols.forEach((z) => {
+                        if (z.symbolName == name) {
+                              ref = z;
+                        }
+                  });
+            });
+      });
+      return ref;
+}
