984bdaa789cf6885ad81a0268dada65a71ef0168
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-02-05 22:22:57 -0500

    Include idle and do not disturb presence statuses

--- functions/online_count.js
+++ functions/online_count.js
@@ -2,9 +2,15 @@ const firebase = require('firebase-admin');
 const Discord = require('discord.js');
 var database = firebase.database();
 
+var onlineStatuses = [
+      'online',
+      'idle',
+      'dnd'
+];
+
 function updateMembers(oldMember, newMember) {
       var guild = newMember.guild;
-      var onlineCount = guild.members.filter(x => x.presence.status === 'online').size;
+      var onlineCount = guild.members.filter(x => onlineStatuses.includes(x.presence.status)).size;
       database.ref('servers/' + guild.id + '/online/states/' + Date.now()).set(onlineCount);
       database.ref('servers/' + guild.id + '/online/record').once('value').then((record) => {
             if (!record.val() || onlineCount > record.val()) {
