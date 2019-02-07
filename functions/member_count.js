19134297f7b4daa856d56fbafd7e53d53f0eef08
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-02-06 21:02:54 -0500

    Add comments
    #4

--- functions/member_count.js
+++ functions/member_count.js
@@ -1,16 +1,25 @@
+// Import stuff
 const firebase = require('firebase-admin');
 const Discord = require('discord.js');
 var database = firebase.database();
 
+// Update recorder server member counts
 function updateMembers(member) {
+      // Get guild from new (or leaving) member
       var guild = member.guild;
+      // Add current server member count and timestamp to list
       database.ref('servers/' + guild.id + '/members/states/' + Date.now()).set(guild.memberCount);
+      // Get current record for most members on server
       database.ref('servers/' + guild.id + '/members/record').once('value').then((record) => {
+            // If record is not already created, create it
+            // If record is broken, update it
             if (!record.val() || guild.memberCount > record.val()) {
+                  // Set servers/server_id/members/record to updated member count
                   database.ref('servers/' + guild.id + '/members/record').set(guild.memberCount);
             }
       });
 }
 
+// Bind event listeners for when guild members join or leave the server
 bot.on('guildMemberAdd', (member) => updateMembers(member));
 bot.on('guildMemberRemove', (member) => updateMembers(member));
